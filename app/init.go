ad7a618b52c36872454da414b422bc6a35507217
董玉岭 <13374034+dong-yuling@user.noreply.gitee.com>
2024-02-27 16:22:26 +0800

    four commit

--- app/init.go
+++ app/init.go
@@ -1,5 +1,15 @@
 package app
 
-func ()  {
+import "github.com/BarnabyCharles/framework/mysql"
 
+func Init(str ...string) error {
+	var err error
+	for _, val := range str {
+		switch val {
+		case "mysql":
+			err = mysql.InitMysql()
+
+		}
+	}
+	return err
 }
